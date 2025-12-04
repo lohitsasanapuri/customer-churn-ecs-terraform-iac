f742bdd9e11a71dce15b8b9238d635691295f087
lohitsasanapuri <lohitsasanapuri@outlook.com>
2025-12-03 18:18:39 -0600

    CI/CD fix

--- backend-resources.tf
+++ backend-resources.tf
@@ -1,53 +1,9 @@
-# S3 bucket for Terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "customer-churn-terraform-state"
-
-  tags = {
-    Name = "terraform-state"
-  }
-}
-
-# Enable versioning (IMPORTANT)
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Enable encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-# DynamoDB for state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "terraform-locks"
-  }
-}
 terraform {
   backend "s3" {
-    bucket         = "customer-churn-terraform-state"
-    key            = "ecs/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket  = "customer-churn-terraform-state"
+    key     = "ecs/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    #profile = "ecs-dev" 
   }
 }
